2c1d9ddb83384f4dc0764059b686dbcdf770762b
noob-express3000 <edujarmainencube@gmail.com>
2026-09-08 23:34:37 +0200

    Bump hardened judge build to v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -177,8 +177,8 @@ android {
         applicationId = "com.shipaton.quotesofwisdom"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
